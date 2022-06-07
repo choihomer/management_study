c0cd63949857b6ea732ee3621286bb8584336cad
choihomer <major@moaland.co.kr>
2022-06-07 12:52:19 +0900

    management_220607_2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import './App.css';
-import logo from './logo.svg' ;
+import Customer from './components/customer' ;
 
+const customers =[{
+  id : 1,
+  image : 'http://placeimg.com/64/64/any/1',
+  name : '홍길동',
+  birthday : '701020',
+  gender : '여자',
+  job : '직장인'
+},
+{
+  id : 21,
+  image : 'http://placeimg.com/64/64/any/2',
+  name : '가나다',
+  birthday : '690310',
+  gender : '남자',
+  job : 'BOSS'
+},
+{
+  id : 3,
+  image : 'http://placeimg.com/64/64/any/3',
+  name : '강건너',
+  birthday : '471234',
+  gender : '여자',
+  job : '무직'
+}]
 function App() {
   return (
-    <div className="gray-background">
-     <img src={logo} lat="logo"/>
-     <h2>Let's start develop management system</h2>
-    </div>
+    <div>
+      {
+        customers.map((a,i)=>{
+          return(
+          <Customer key={i}
+                  id={customers[i].id}
+                  image={customers[i].image}
+                  name={customers[i].name}
+                  birthday={customers[i].birthday}
+                  gender={customers[i].gender}
+                  job={customers[i].job}/>)
+        })
+      }
+      </div>
   );
 }
 
